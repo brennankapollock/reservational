2439a3eca4da24a00f4bf224b4988a066fdfe917
brennankapollock <brennankapollock@gmail.com>
2021-06-11 23:06:05 -0500

    added migrations/seeds

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -7,61 +7,52 @@
 require('dotenv').config();
 const path = require("path");
 
-const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
-  DEBUG,
-} = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL_DEVELOPMENT,
+    connection: "postgres://oxwhtlwk:c4T1UcoocHcKwyhbNty60SXWfXDrtNQD@batyr.db.elephantsql.com/oxwhtlwk",
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    debug: !!DEBUG,
+    
   },
   test: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL_TEST,
+    connection: "",
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    debug: !!DEBUG,
+    
   },
   preview: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL_PREVIEW,
+    connection: "",
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    debug: !!DEBUG,
   },
   production: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL,
+    connection: "",
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    debug: !!DEBUG,
   },
 };
---
+++ back-end/src/db/fixtures/reservations.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    first_name: "Rick",
+    last_name: "Sanchez",
+    mobile_number: "202-555-0164",
+    reservation_date: "2020-12-31",
+    reservation_time: "20:00:00",
+    people: 6,
+  },
+  {
+    first_name: "Frank",
+    last_name: "Palicky",
+    mobile_number: "202-555-0153",
+    reservation_date: "2020-12-30",
+    reservation_time: "20:00",
+    people: 1,
+  },
+  {
+    first_name: "Bird",
+    last_name: "Person",
+    mobile_number: "808-555-0141",
+    reservation_date: "2020-12-30",
+    reservation_time: "18:00",
+    people: 1,
+  },
+  {
+    first_name: "Tiger",
+    last_name: "Lion",
+    mobile_number: "808-555-0140",
+    reservation_date: "2025-12-30",
+    reservation_time: "18:00",
+    people: 3,
+  },
+  {
+    first_name: "Anthony",
+    last_name: "Charboneau",
+    mobile_number: "620-646-8897",
+    reservation_date: "2026-12-30",
+    reservation_time: "18:00",
+    people: 2,
+  }
+]
